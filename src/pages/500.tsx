bd8b192ed7ba7619ef7d2a61c477a1de1e125a5c
yamidduarte <yamid9@gmail.com>
2023-03-25 04:23:25 -0700

    cleared

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -17,54 +17,7 @@ const InternalServerErrorPage: React.FC<{
 }> = ({ locale }) => {
   return (
     <>
-      <Head>
-        <title key="title">{`An error occurred - ${configuration.site.name}`}</title>
-        <PagesDirectoryFonts />
-      </Head>
-
-      <SiteHeader />
-
-      <I18nProvider lang={locale}>
-        <div
-          className={
-            'm-auto flex min-h-[50vh] w-full items-center justify-center'
-          }
-        >
-          <div className={'flex flex-col space-y-8'}>
-            <div className={'flex space-x-8 divide-x divide-gray-100'}>
-              <div>
-                <Heading type={1}>
-                  <span className={'text-primary-500'}>500</span>
-                </Heading>
-              </div>
-
-              <div className={'flex flex-col space-y-4 pl-8'}>
-                <div className={'flex flex-col space-y-2'}>
-                  <div>
-                    <Heading type={1}>
-                      <Trans i18nKey={'common:genericError'} />
-                    </Heading>
-                  </div>
-
-                  <p className={'text-gray-500 dark:text-gray-300'}>
-                    <Trans i18nKey={'common:genericErrorSubHeading'} />
-                  </p>
-                </div>
-
-                <div className={'flex space-x-4'}>
-                  <Button color={'secondary'} href={'/'}>
-                    <Trans i18nKey={'common:contactUs'} />
-                  </Button>
-
-                  <Button href={'/'}>
-                    <Trans i18nKey={'common:backToHomePage'} />
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </I18nProvider>
+      
     </>
   );
 };
