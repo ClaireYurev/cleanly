e5e584986f6a727fe5b4a4b079416b8f1fe183e2
yamidduarte <yamid9@gmail.com>
2023-03-25 04:54:27 -0700

    import { PagesDirectoryFonts } from '~/components/Fonts';

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,7 +8,7 @@ import Heading from '~/core/ui/Heading';
 import Trans from '~/core/ui/Trans';
 
 import SiteHeader from '~/app/(site)/components/SiteHeader';
-import { PagesDirectoryFonts } from '../components/Fonts';
+import { PagesDirectoryFonts } from '~/components/Fonts';
 import I18nProvider from '~/i18n/I18nProvider';
 import configuration from '../configuration';
 
