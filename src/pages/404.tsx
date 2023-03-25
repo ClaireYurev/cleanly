eff90673b33ef8e23e3521d6280c57baa69224f4
yamidduarte <yamid9@gmail.com>
2023-03-25 04:40:48 -0700

    Cut <PagesDirectoryFonts /> only

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import '~/app/globals.css';
+import '../app/globals.css';
 
 import Head from 'next/head';
 import { GetStaticPropsContext } from 'next';
@@ -23,6 +23,54 @@ const NotFoundPage: React.FC<{
         <title key="title">{`Page not found - ${configuration.site.name}`}</title>
       </Head>
 
+      <SiteHeader />
+
+      <I18nProvider lang={locale}>
+        <div
+          className={
+            'm-auto flex min-h-[50vh] w-full items-center justify-center'
+          }
+        >
+          <div className={'flex flex-col space-y-8'}>
+            <div className={'flex space-x-8 divide-x divide-gray-100'}>
+              <div>
+                <Heading type={1}>
+                  <span
+                    data-cy={'catch-route-status-code'}
+                    className={'text-primary-500'}
+                  >
+                    404
+                  </span>
+                </Heading>
+              </div>
+
+              <div className={'flex flex-col space-y-4 pl-8'}>
+                <div className={'flex flex-col space-y-2'}>
+                  <div>
+                    <Heading type={1}>
+                      <Trans i18nKey={'common:pageNotFound'} />
+                    </Heading>
+                  </div>
+
+                  <p className={'text-gray-500 dark:text-gray-300'}>
+                    <Trans i18nKey={'common:pageNotFoundSubHeading'} />
+                  </p>
+                </div>
+
+                <div className={'flex space-x-4'}>
+                  <Button color={'secondary'} href={'/'}>
+                    <Trans i18nKey={'common:contactUs'} />
+                  </Button>
+
+                  <Button href={'/'}>
+                    <Trans i18nKey={'common:backToHomePage'} />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </I18nProvider>
     </>
   );
 };
